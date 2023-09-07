12d88d756c4c6182369d62d93841507ba4e309a6
JCNoxoia <jcbarrett2000@protonmail.com>
2023-09-07 18:15:29 -0500

    removed previous code and created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello World!');
-let favoriteFood = 'Spicy Chicken & Broccoli Stirfry';
-document.write(favoriteFood);
+pokemonList = [
+    {name: 'pikachu', height: 0.4, type: ['electric']},
+    {name: 'mimikyu', height: 0.2, type: ['ghost', 'fairy']},
+    {name: 'squirtle', height: 0.5, type: ['water']},
+    {name: 'jigglypuff', height: 0.5, type: ['normal', 'fairy']},
+    {name: 'luxray', height: 1.4, type: ['electric']},
+    {name: 'cyndaquil', height: 0.5, type: ['fire']},
+    {name: 'mareep', height: 0.6, type: ['electric']},
+    {name: 'beedrill', height: 1, type: ['bug', 'poison']}
+];
