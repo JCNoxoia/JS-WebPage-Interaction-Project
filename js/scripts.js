218070cc54d7328d8a509179ac49520bf4d105b1
JCNoxoia <jcbarrett2000@protonmail.com>
2023-10-10 20:22:03 -0500

    made slight edits to style, added the hidePokeModal function and added event listeners to open and close modal at appropriate times

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ let pokemonRepository = (function() {
     function showDetails(pokemon) {
       loadDetails(pokemon).then(function () {
         console.log(pokemon);
+        showPokeModal(pokemon.imageUrl, pokemon.name, pokemon.height, pokemon.types);
       });
     }
 
@@ -25,11 +26,72 @@ let pokemonRepository = (function() {
         })
     }
 
-//Function called to show modal on pokemon button press    
-    function showPokeModal() {
-      let modal = document.querySelector('#poke-banner');
-      modal.classList.add('is-visible');
+//Functions to hide modal on close button, esc key, and click on outside of modal
+    function hidePokeModal() {
+      let modalContainer = document.querySelector('#poke-banner');
+      modalContainer.classList.remove('is-visible');
     }
+
+    // window.addEventListener('keydown', (e) => {
+    //   let modalContainer = document.querySelector('#poke-banner');
+    //   if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    //     hidePokeModal();  
+    //   }
+    // });
+
+//Function called to show modal on pokemon button press    
+    function showPokeModal(spriteUrl, name, height, types) {
+      let modalContainer = document.querySelector('#poke-banner');
+      modalContainer.innerHTML = '';
+
+      let modalImage = document.createElement('div');
+      modalImage.classList.add('poke-banner__image-container');
+
+      let spriteBox = document.createElement('div');
+      spriteBox.classList.add('sprite-box');
+
+      let pokeSprite = document.createElement('img');
+      pokeSprite.src = spriteUrl;
+      pokeSprite.width = 300;
+      pokeSprite.height = 300;
+      
+      let modalText = document.createElement('div');
+      modalText.classList.add('poke-banner__text-container');
+
+      let pokeDescription = document.createElement('p');
+      pokeDescription.innerText = name + height + types;
+
+      let closeButton = document.createElement('button');
+      closeButton.classList.add('modal-close');
+      closeButton.innerText = 'Close';
+      closeButton.addEventListener('click', hidePokeModal);
+
+      modalImage.appendChild(spriteBox);
+      spriteBox.appendChild(pokeSprite);
+      modalText.appendChild(pokeDescription);
+      modalContainer.appendChild(modalImage);
+      modalContainer.appendChild(modalText);
+      modalContainer.appendChild(closeButton);
+
+      modalContainer.classList.add('is-visible');
+
+      screenContainer = document.querySelector('#screen-container');
+      screenContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === screenContainer) {
+          hidePokeModal();
+        };
+      });
+
+      welcomeBanner = document.querySelector('#welcome-banner');
+      welcomeBanner.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === welcomeBanner) {
+          hidePokeModal();
+        };
+      });
+    }
+
 
 // loop to add pokemon buttons to DOM and log them in console
     function addListItem(pokemon) {
@@ -45,11 +107,6 @@ let pokemonRepository = (function() {
         pmListBlock.appendChild(pmListBlockItem);
 
         logDetails(pokeButton, pokemon);
-
-        //Event listener for modal
-        pokeButton.addEventListener('click', function() {
-          showPokeModal();
-        })
     }
 
     function loadList() {
